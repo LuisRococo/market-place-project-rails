0839ab75778aa881efa31870e2f3349fdb040997
Luis Roberto <luisrobertopenav@gmail.com>
2022-12-08 10:08:28 -0700

    add new product page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'products#index'
 
+  get '/products/new', to: 'products#new', as: 'new_product'
   get 'products/:id', to: 'products#show', as: 'product'
   get '/products', to: 'products#index'
 end
