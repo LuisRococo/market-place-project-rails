661e35910f09fbb74a848464c8c5bafb071f7f07
Luis Roberto <luisrobertopenav@gmail.com>
2022-12-08 12:48:50 -0700

    add create product logic

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/products/new', to: 'products#new', as: 'new_product'
   get 'products/:id', to: 'products#show', as: 'product'
   get '/products', to: 'products#index'
+  post '/products', to: 'products#create'
 end
