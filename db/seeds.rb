864b56d78c7b96f1d84c4a6c07eaa92209053777
Luis Roberto <luisrobertopenav@gmail.com>
2022-12-09 12:00:25 -0700

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,46 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(
+    [
+        {
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'JOHN@example.com',
+            password: 'root'
+        },
+        {
+            first_name: 'John',
+            last_name: 'Doe',
+            email: 'jane@example.com',
+            password: 'root'
+        }
+    ]
+)
+
+john = User.first
+jane = User.last
+
 products = Product.create([
     {
         name: 'Name 1',
         price: '50.99',
-        description: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        description: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        quantity: 5,
+        user_id: john.id
     },
     {
         name: 'Name 2',
         price: '50.99',
-        description: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        description: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        quantity: 5,
+        user_id: jane.id
     },
     {
         name: 'Name 3',
         price: '50.99',
-        description: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        description: 'Lorem ipsum dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        quantity: 5,
+        user_id: jane.id
     }
 ])
--- docs/general_notes.md
+++ docs/general_notes.md
@@ -3,6 +3,7 @@
 - rails routes -c products
 - rails c --sandbox (when closing the console it will rollback all transactions)
 - rails g migration add_user_id_and_quantity_to_products user:references quantity:integer
+- rails db:reset -> drop database, create it again, and seed it
 
 ## METHODS
 
@@ -10,3 +11,4 @@
 - get 'signup', to: redirect('users/new')
 - object.class.name.humanize.downcase -> name of class
 - has_many :products, dependent: :destroy -> if deleted then it will delete products
+- User.create!(...) -> throws error on fail
