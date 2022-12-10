68797369e05932f9db3bc27b116ef60c93712d61
Luis Roberto <luisrobertopenav@gmail.com>
2022-12-10 13:53:59 -0700

    add page component placeholders fo index page

--- app/javascript/containers/App.jsx
+++ app/javascript/containers/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Jumbotron from "../components/products/Jumbotron";
+import ProductsContainer from "../components/products/ProductsContainer";
+import Header from "../components/shared/Header";
+import Footer from "../components/shared/Footer";
 
 const App = ()=> {
     return (
-        <div>App Component</div>
+        <div>
+            App Component
+             <Header />
+             <Jumbotron />
+             <ProductsContainer />
+             <Footer />
+        </div>
+       
     )
 }
 
