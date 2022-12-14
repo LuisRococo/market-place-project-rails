66555642fb5caf5268a4343d0dd46b45f650c014
Luis Roberto <luisrobertopenav@gmail.com>
2022-12-13 19:06:14 -0700

    add not found route

--- app/javascript/containers/App.jsx
+++ app/javascript/containers/App.jsx
@@ -13,6 +13,21 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<ProductsContainer />} />
+          <Route
+            path="*"
+            element={
+              <div className="container">
+                <div className="row">
+                  <div className="card col-md-8 offset-md-2 mt-4 form-header-style">
+                    <h1 className="text-center m-4">404: Not Found</h1>
+                    <p className="text-center m-4">
+                      The resource you are looking for, could not be found
+                    </p>
+                  </div>
+                </div>
+              </div>
+            }
+          />
         </Routes>
         <Footer />
       </>
